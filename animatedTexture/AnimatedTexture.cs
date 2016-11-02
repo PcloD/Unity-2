a4283ab642877168f73d7c1e0e6d0cb0e3ebe7ed
Simon de Bakker <simon@simbits.nl>
2016-11-02 20:45:02 +0100

    some comments

--- animatedTexture/AnimatedTexture.cs
+++ animatedTexture/AnimatedTexture.cs
@@ -5,35 +5,34 @@ using System.IO;
 
 
 public class AnimatedTexture : MonoBehaviour {
-	public Material mainmaterial;
-	public string path;
-	public string extension = "png";
+    public Material mainmaterial;        /* material to apply the texture to */
+    public string path;                  /* path where the files can be found */
+    public string extension = "png";     /* extension of the files to load */
 
-	private int counter;
-	private List<Texture2D> textureList = new List<Texture2D>();
+    private int counter;
+    private List<Texture2D> textureList = new List<Texture2D>();
 
+    void Start () {
+        /* get all files in 'path' ending in 'extension' */
+        DirectoryInfo dir = new DirectoryInfo(path);
+        FileInfo[] info = dir.GetFiles("*." + extension);
 
-	// Use this for initialization
-	void Start () {
-		DirectoryInfo dir = new DirectoryInfo(path);
+        /* load each file as texture and add to the texture list */
+        foreach (FileInfo f in info) {
+            WWW file = new WWW ("file://" + f.FullName); /* use WWW to load files none resource files */
+            textureList.Add (file.texture);              /* append to the list */
+        }
 
-		FileInfo[] info = dir.GetFiles("*." + extension);
+        InvokeRepeating ("Texturechanger", 0, .05f);
+    }
 
-		foreach (FileInfo f in info) {
-			WWW file = new WWW ("file://" + f.FullName);
-			textureList.Add (file.texture);
-		}
+    // Update is called once per frame
+    void Update () {
 
-		InvokeRepeating ("Texturechanger", 0, .05f);
-	}
+    }
 
-	// Update is called once per frame
-	void Update () {
-
-	}
-
-	void Texturechanger()
-	{
+    void Texturechanger()
+    {
         mainmaterial.SetTexture ("_MainTex", textureList [counter]);
         if (++counter >= textureList.Count) {
             counter = 0;
